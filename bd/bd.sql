4a27f7f95c189d2fdc41b5fe3c7ecf2a2ce3fb10
Alan Tavares <alan_tj@hotmail.com.br>
2018-05-15 07:35:00 -0300

    Banco de dados

---
+++ bd/bd.sql
@@ -0,0 +1,22 @@
+
+create database seridoeventos;
+use seridoeventos;
+
+create table usuario(
+	id int auto_increment not null,
+	nome varchar(200) not null,
+	email varchar(200) not null,
+	senha varchar(200) not null,
+	primary key(id)
+);
+create table denuncia(
+	id int auto_increment not null,
+	data_recebimento text not null,
+	nome_reclamante text not null,
+	nome_reclamado text not null,
+	endereco_reclamante text not null,
+	endereco_reclamado text not null,
+	motivo text not null,
+	medidas text not null,
+	primary key(id)
+);
